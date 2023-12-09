982f3be19217aa605f1f78ba1cf23bf6e251a6c9
simo <wambuisimon712@gmail.com>
2023-12-09 16:41:40 +0300

    first commit

--- Frontend/src/components/Main.jsx
+++ Frontend/src/components/Main.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import emptyCV from "./utils/emptyCV";
 import CVForm from "./CVForm/CVForm";
 
 const Main = () => {
+  const [cv, setCv] = useState(emptyCV);
+
+  const handleChangePersonal = (e) => {
+    const { name, value, type } = e.target;
+
+    if (type === "file") {
+      handleChangeFile(e);
+      return;
+    }
+
+    setCv((prevState) => ({
+      ...prevState,
+      personalInfo: {
+        ...prevState.personalInfo,
+        [name]: value,
+      },
+    }));
+  };
+
   return (
     <div>
-      <CVForm />
+      <CVForm handleChangePersonal={handleChangePersonal} />
     </div>
   );
 };
--- Frontend/src/components/utils/emptyCV.jsx
+++ Frontend/src/components/utils/emptyCV.jsx
@@ -1,14 +1,38 @@
 import { v4 as uuidv4 } from "uuid";
+import emptyAvatar from "../../assets/empty_avatar.png";
 
 const emptyCV = {
   personalInfo: {
     firstName: "",
-    firstName: "",
-    firstName: "",
-    photo: "",
+    lastName: "",
+    title: "",
+    photo: emptyAvatar,
     address: "",
     phoneNumber: "",
     email: "",
     description: "",
   },
+  experience: [
+    {
+      id: uuidv4(),
+      position: "",
+      company: "",
+      city: "",
+      from: "",
+      to: "",
+    },
+  ],
+  education: [
+    {
+      id: uuidv4(),
+      universityName: "",
+      city: "",
+      degree: "",
+      subject: "",
+      from: "",
+      to: "",
+    },
+  ],
 };
+
+export default emptyCV;
