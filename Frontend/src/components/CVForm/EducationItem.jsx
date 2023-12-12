1f12a58f9de94cfa8750e18e3d21fa988672ec89
simo <wambuisimon712@gmail.com>
2023-12-12 09:11:24 +0300

    first commit

--- Frontend/src/components/CVForm/EducationItem.jsx
+++ Frontend/src/components/CVForm/EducationItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Input from "../Utils/Input";
+import Button from "../Utils/Button";
 
-const EducationItem = () => {
-  return <div>EducationItem</div>;
+const EducationItem = ({ id, EducationItem, onChange, onDelete }) => {
+  return (
+    <>
+      <Input />
+      <Button></Button>
+    </>
+  );
 };
 
 export default EducationItem;
